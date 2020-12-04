69336862516d689b7850411ec07c59d3383f68dd
Arifia Kasastra R <arka.progammer@gmail.com>
2020-12-04 16:35:43 +0700

    Add pagination, limit and search in GET Users

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,99 +11,129 @@ const userSchema = Joi.object().keys({
     password: Joi.string().min(8).max(20).required()
 });
 
+const getPagination = (page, size) => {
+    const limit = size ? +size : 3;
+    const offset = page ? page * limit : 0;
+  
+    return { limit, offset };
+};
+
+const getPagingData = (data, page, limit) => {
+    const { count: totalItems, rows: portfolios } = data;
+    const currentPage = page ? +page : 0;
+    const totalPages = Math.ceil(totalItems / limit);
+  
+    return { data: portfolios, currentPage, totalData: totalItems, pageSize: limit, totalPages  };
+};
+
 let controllers = {
     get: async function(res, req, next){
-        const users = await models.users.findAll({
-            attributes: {exclude: ['password', 'createdAt', 'updatedAt']},
-        });
-        
-        try {
-            res.set({
-              'Content-Type': 'application/json'
+      const { page, size, search } = req.query;
+      const { limit, offset } = getPagination(page, size);
+      const issearch = search ? { 
+          [Op.or]: [
+              {name: {[Op.like]: `%${search}%`}},
+              {username: {[Op.like]: `%${search}%`}}
+          ]
+        } : null;
+
+        res.set({
+            'Content-Type': 'application/json'
+        })
+
+        await models.users
+            .findAndCountAll({
+                where: issearch,
+                limit,
+                offset,
+                attributes: {exclude: ['password', 'createdAt', 'updatedAt']},
             })
-            if(users.length!==0) {
-              res.json({
-                'status': 200,
-                'message': 'Success',
-                'response': users
-              })
-            } else {
-              res.json({
-                'status': 204,
-                'message': 'Unavailable',
-                'errors': 'Data Unavailable'
-              })
-            }
-        } catch (error) {
-            res.status(500).json({
-              'status': '500',
-              'message': 'Service Unavailable',
-              'error': error
+            .then( users => {
+                if(users.length!==0) {
+                    const response = getPagingData(users, page, limit);
+                    res.json({
+                        'status': 200,
+                        'message': 'Success',
+                        'response': response
+                    })
+                } else {
+                    res.json({
+                        'status': 204,
+                        'message': 'Unavailable',
+                        'errors': 'Data Unavailable'
+                    })
+                }
             })
-        }
-    },
+            .catch( err => {
+                res.status(500).json({
+                    'status': '500',
+                    'message': 'Service Unavailable',
+                    'error': err.message
+                })
+            })
+        },
     post: async function(res, req, next){
         const {name, username, password} = req.body;
         const data = req.body;
       
         try {
-          res.set({
-            'Content-Type': 'application/json'
-          })
-        
-          const {error, value} = userSchema.validate(data);
-          
-          if(error) {
-            res.status(400).json({
-              'status': 400,
-              'message': 'Error',
-              'error': {
-                'field': error.details[0].path[0],
-                'key': error.details[0].path[0]+'.'+error.details[0].type,
-                'message': error.details[0].message
-              }
-            });
-            res.end();
-          }
-      
-          // Check Username Taken
-          const users = await models.users.findAll({
-            where: {
-              username: username
-            },
-            attributes: ['id']
-          });
-          
-          if(users.length!=0) {
-            res.status(400).json({
-              'status': 400,
-              'message': 'Failed',
-              'error': {
-                "field": "username",
-                "key": "username.unique",
-                "message": "Username Already Taken"
-              }
-            });
-            res.end()
-          }
-      
-          let newUser = await models.users.create({
-            name: name,
-            username: username,
-            password: await hash(password)
-          })
-          var token = jwt.sign({ 
-            id: newUser.id
-          }, '@Sportfolio@');
-          
-          res.status(200).json({
-            'status': '200',
-            'message': 'Success',
-            'response': {
-              'name': name,
-              'token': token
+            res.set({
+                'Content-Type': 'application/json'
+            })
+            
+            const {error, value} = userSchema.validate(data);
+            
+            if(error) {
+                res.status(400).json({
+                'status': 400,
+                'message': 'Error',
+                'error': {
+                    'field': error.details[0].path[0],
+                    'key': error.details[0].path[0]+'.'+error.details[0].type,
+                    'message': error.details[0].message
+                }
+                });
+                res.end();
             }
-          });
+        
+            // Check Username Taken
+            const users = await models.users.findAll({
+                where: {
+                    username: username
+                },
+                attributes: ['id']
+            });
+            
+            if(users.length!=0) {
+                res.status(400).json({
+                'status': 400,
+                'message': 'Failed',
+                'error': {
+                    "field": "username",
+                    "key": "username.unique",
+                    "message": "Username Already Taken"
+                }
+                });
+                res.end()
+            }
+        
+            let newUser = await models.users.create({
+                name: name,
+                username: username,
+                password: await hash(password)
+            })
+            var token = jwt.sign({ 
+                id: newUser.id
+            }, '@Sportfolio@');
+            
+            res.status(200).json({
+                'status': '200',
+                'message': 'Success',
+                'response': {
+                    'name': name,
+                    'token': token
+                }
+            });
       
         } catch (error) {
           res.status(500).json({
