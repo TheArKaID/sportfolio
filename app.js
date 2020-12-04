043798b392b0c40ed0a49423ea740247577e9bd9
Arifia Kasastra R <arka.progammer@gmail.com>
2020-12-04 23:03:55 +0700

    Fix using swagger.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var swaggerUi = require('swagger-ui-express');
-var swaggerDocument = require('./swagger.json');
+var swaggerDocument = require('./swagger.js');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
